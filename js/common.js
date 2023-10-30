b8ae8a08c64c418be82def152e7d528c302936c2
santosopy <santoso@tsart.jp>
2023-10-30 14:40:59 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(function () {
+    // backtotop
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
